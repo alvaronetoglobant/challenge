226de47bfea17d98d705408e62d08919fb672d21
alvaronetoglobant <106993538+alvaronetoglobant@users.noreply.github.com>
2022-10-28 09:00:03 -0300

    Add files via upload

--- main.tf
+++ main.tf
@@ -30,6 +30,13 @@ resource "azurerm_subnet" "internal" {
   address_prefixes     = ["10.0.2.0/24"]
 }
 
+resource "azurerm_public_ip" "challenge-ip" {
+  name                = "pip-challenge"
+  resource_group_name = azurerm_resource_group.challenge.name
+  location            = azurerm_resource_group.challenge.location
+  allocation_method   = "Dynamic"
+}
+
 resource "azurerm_network_interface" "challenge" {
   name                = "${var.name}-nic"
   location            = azurerm_resource_group.challenge.location
@@ -39,7 +46,44 @@ resource "azurerm_network_interface" "challenge" {
     name                          = "challengeconfiguration1"
     subnet_id                     = azurerm_subnet.internal.id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.challenge-ip.id
   }
+}
+
+# Create Network Security Group and rule
+resource "azurerm_network_security_group" "challenge-nsg" {
+  name                = "challenge-nsg"
+  location            = azurerm_resource_group.challenge.location
+  resource_group_name = azurerm_resource_group.challenge.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "200.158.169.179"
+    destination_address_prefix = "*"
+  }
+  security_rule {
+    name                       = "HTTP"
+    priority                   = 1010
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "8080"
+    source_address_prefix      = "200.158.169.179"
+    destination_address_prefix = "*"
+  }
+}
+
+# Connect the security group to the network interface
+resource "azurerm_network_interface_security_group_association" "challenge-association" {
+  network_interface_id      = azurerm_network_interface.challenge.id
+  network_security_group_id = azurerm_network_security_group.challenge-nsg.id
 }
 
 resource "azurerm_virtual_machine" "challenge" {
@@ -54,7 +98,7 @@ resource "azurerm_virtual_machine" "challenge" {
   storage_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "20.04-LTS"
+    sku       = "18.04-LTS"
     version   = "latest"
   }
   storage_os_disk {
@@ -65,8 +109,8 @@ resource "azurerm_virtual_machine" "challenge" {
   }
   os_profile {
     computer_name  = "challenge-devops"
-    admin_username = "challenge"
-    admin_password = "challenge$"
+    admin_username = "azroot"
+    admin_password = "seguro@12345"
   }
   os_profile_linux_config {
     disable_password_authentication = false
